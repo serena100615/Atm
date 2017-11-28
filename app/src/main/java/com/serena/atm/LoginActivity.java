eb40cf086e43ae575f147b496806b0eca971f00f
serena100615 <serena100615@gmail.com>
2017-11-28 11:59:38 +0800

    LoginSecond

--- app/src/main/java/com/serena/atm/LoginActivity.java
+++ app/src/main/java/com/serena/atm/LoginActivity.java
@@ -1,7 +1,11 @@
 package com.serena.atm;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +13,26 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+    }
+    public void Login (View view){
+        EditText userid = (EditText) findViewById(R.id.userid);
+        EditText passwd = (EditText) findViewById(R.id .passwd);
+        String uid = userid.getText().toString();
+        String pawd = passwd.getText().toString();
+        if("jack".equals(uid) && "1234".equals(pawd)){
+            Toast.makeText(this,"登入成功",Toast.LENGTH_LONG).show();
+            finish();
+        }else{
+            new AlertDialog.Builder(this)
+                    .setTitle("Atm")
+                    .setMessage("登入失敗")
+                    .setPositiveButton("OK",null)
+                    .show();
+
+        }
+    }
+    public void cancel (View view){
+
     }
 }
